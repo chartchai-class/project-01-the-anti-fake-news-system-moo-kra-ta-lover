4f5dffea176428a9e2a3c8ce27e3ab04d83290dc
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-08-18 00:24:51 +0700

    chore(style): add custom font to the project

--- moo_kra_ta_news/tailwind.config.ts
+++ moo_kra_ta_news/tailwind.config.ts
@@ -6,8 +6,12 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sumana': ['Sumana', 'serif'],
+        'inter': ['Inter', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 } satisfies Config
-
