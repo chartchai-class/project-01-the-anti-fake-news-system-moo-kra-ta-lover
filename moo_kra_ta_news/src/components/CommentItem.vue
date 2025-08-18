5584d9db3f026e073fd3fe4ef0ccc8c05070eac0
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2025-08-18 21:22:21 +0700

    refactor(mockdata): change where to mockdata is hosted

--- moo_kra_ta_news/src/components/CommentItem.vue
+++ moo_kra_ta_news/src/components/CommentItem.vue
@@ -1,19 +1,27 @@
 <script setup lang="ts">
 import type { Comment } from '@/types';
-defineProps<{
+import { getUserProfile } from '@/utils/userProfile';
+import { computed } from 'vue';
+const props = defineProps<{
   comment: Comment;
 }>();
+
+const userProfile = computed(() => getUserProfile(props.comment.user|| ''));
 </script>
 
 
 <template>
     <div>
         <div class="border border-grey-600 bg-[#F5F5F5] p-4 mb-6 hover:scale-101 hover:shadow-sp w-full rounded-lg">
-            <div class="flex items-center gap-2 font-inter">
-                <div class="rounded-full w-8 h-8 bg-indigo-500 flex items-center justify-center text-xl text-white">{{ comment.user.charAt(0).toUpperCase() }}</div>
-                <span><p>{{ comment.user }}</p></span>
+            <div class="flex items-center justify-between font-inter">
+                <div class="flex items-center gap-2 font-inter">
+                    <div :class="[userProfile.bgColor, 'rounded-full w-8 h-8 flex items-center justify-center text-xl text-white']">
+                        {{ userProfile.initials }}
+                    </div>
+                    <span>{{ comment.user }}</span>
+                </div>
+                <span>{{ comment.vote }}</span>
             </div>
-            <p>{{ comment.vote }}</p>
             <img 
             v-if="comment.imageUrl && comment.imageUrl.length > 0" 
             :src="comment.imageUrl[0]" 
--- moo_kra_ta_news/src/services/NewsService.ts
+++ moo_kra_ta_news/src/services/NewsService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Eakkasit662115058/Moo_Kra_ta_news_database',
+  baseURL: 'https://68a31457c5a31eb7bb1edde1.mockapi.io/',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
