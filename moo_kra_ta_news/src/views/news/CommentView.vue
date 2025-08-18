d7c1f1b5116e2bd1a26b41fb6a913bee44d6655d
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2025-08-18 17:25:12 +0700

    style(detailpage): change detailpage style on picture details and vote button

--- moo_kra_ta_news/src/views/news/CommentView.vue
+++ moo_kra_ta_news/src/views/news/CommentView.vue
@@ -1,10 +1,52 @@
 <script setup lang="ts">
 import CommentItem from '@/components/CommentItem.vue';
+import NewsService from '@/services/NewsService';
+import type { News } from '@/types';
+import { computed, onMounted, ref } from 'vue';
+const news = ref<News | null>(null);
+const id = ref<number>(1);
+
+const totalComments = computed(() => {
+  return news.value?.comments?.length || 0;
+});
+
+const realComments = computed(() => {
+  return news.value?.comments?.filter(comment => comment.vote === 'Real').length || 0;
+});
+
+const fakeComments = computed(() => {
+  return news.value?.comments?.filter(comment => comment.vote === 'Fake').length || 0;
+});
+
+onMounted(async () => {
+  try {
+    const response = await NewsService.getNewsById(id.value);
+    news.value = response.data;
+    console.log(news.value);
+  } catch (error) {
+    console.error('Error fetching news by ID:', error);
+  }
+});
+
 </script>
 
 <template>
-    this is commentView
-    <div>
-        <CommentItem v-for="n in 10" :key="n"/>
+  <div>
+    <div class="my-6 gap-5 flex items-center">
+      <span class="font-inter text-2xl">Comment</span> 
+      <div class="h-6 w-8 rounded-full bg-[#F5F5F5] flex items-center justify-center">
+        <span class="text-base font-inter">{{ totalComments }}</span>
+      </div>
     </div>
+
+    <div class="my-6">
+      <p class="font-inter text-xl">Real Comments ({{ realComments }})  Fake Comments ({{ fakeComments }})</p>
+    </div>
+
+    <div v-if="news && news.comments">
+      <CommentItem 
+        v-for="comment in news.comments" :key="comment.id" :comment="comment"
+      />
+    </div>
+  </div>
 </template>
--- moo_kra_ta_news/src/views/news/DetailView.vue
+++ moo_kra_ta_news/src/views/news/DetailView.vue
@@ -31,30 +31,39 @@ function formatDate(dateStr: string): string {
 
   return `${weekday} ${month} ${day}, ${year}`;
 }
+
 </script>
 <template>
 
     <div v-if="news">
         <img 
-        v-if="news.imageUrl && news.imageUrl.length > 0" 
-        :src="news.imageUrl[0]" 
-        alt="News Image" 
+            v-if="news.imageUrl && news.imageUrl.length > 0" 
+            :src="news.imageUrl[0]" 
+            alt="News Image" class="w-full h-90 object-cover mx-auto my-6"
         />
+    </div>
 
-        <h1>{{ news.topic }}</h1>
+    <div class="mx-auto max-w-7xl lg:px-8">
+    <div v-if="news">
+        <div class="my-6">
+            <h1 class="font-sumana text-4xl font-bold">{{ news.topic }}</h1>
+        </div>
 
-        <div class="flex items-center gap-2">
-            <div class="rounded-full w-10 h-10 bg-indigo-500 flex items-center justify-center text-3xl text-white">{{ news.reporter.charAt(0).toUpperCase() }}</div>
+
+        <div class="flex items-center gap-2 font-inter my-6">
+            <div class="rounded-full w-8 h-8 bg-indigo-500 flex items-center justify-center text-xl text-white">{{ news.reporter.charAt(0).toUpperCase() }}</div>
             <span>{{ news.reporter }} • {{ formatDate(news.reportDate) }}</span>
         </div>
 
-        <p>{{ news.fullDetail }}</p>
+        <div class="my-6">
+            <p class="font-inter text-xl">{{ news.fullDetail }}</p>
+        </div>
 
     </div>
 
     <RouterLink to="/Vote">
-    <div class="m-8">
-        <button class="flex p-6 justify-between items-center w-full bg-[#F5F5F5] rounded-lg border border-gray-200 shadow-sm hover:shadow-md hover:bg-gray-50 transition-all">
+    <div class="my-8">
+        <button class="flex p-4 justify-between items-center w-full bg-[#F5F5F5] rounded-lg border border-gray-200 shadow-sm hover:shadow-md hover:bg-gray-50 transition-all">
             
             <div class="flex-shrink-0 rounded-full p-4 bg-white">
                 <img src="@/assets/Text_logo.svg" alt="" class="w-6 h-6">
@@ -62,8 +71,8 @@ function formatDate(dateStr: string): string {
     
 
             <div class="flex-1 text-left ml-4">
-                <h3 class="font-semibold text-gray-900">Share Your Opinion With Us</h3>
-                <p class="text-sm text-gray-500 mt-1">Vote whether the news is fake or not and comment your opinion.</p>
+                <h3 class="font-semibold text-gray-900 font-inter text-lg">Share Your Opinion With Us</h3>
+                <p class="text-base text-gray-500 mt-1 font-inter">Vote whether the news is fake or not and comment your opinion.</p>
             </div>
     
 
@@ -74,7 +83,10 @@ function formatDate(dateStr: string): string {
     </div>
     </RouterLink>
         
+    
+    <CommentView/>
 
-    <CommentView />
 
+
+    </div>
 </template>
