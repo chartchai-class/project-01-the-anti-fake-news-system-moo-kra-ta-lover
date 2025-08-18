1ddab9eb19f6f03ed679fdc7980ba021f52eabc8
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-08-18 16:52:19 +0700

    refactor(newsdetail): update imageUrl type for proper use

--- moo_kra_ta_news/src/components/NewsCard.vue
+++ moo_kra_ta_news/src/components/NewsCard.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
 import { type News } from '@/types';
+import { formatDate } from '@/utils/dateFormatter';
 defineProps<{
   news: News;
 }>()
 </script>
 
 <template>
-  <div class="event-class">
-  <RouterLink class="event-link" :to=" { name: 'event-detail-view', params: { id: news.id } }">
-    <div class="cursor-pointer border border-grey-600 p-4 w-64 mb-6 hover:scale-101 hover:shadow-sp">
+  <!-- <RouterLink class="event-link" :to=" { name: 'event-detail-view', params: { id: news.id } }"> -->
+    <div class="cursor-pointer w-full mb-6 hover:scale-101 hover:shadow-sp">
+      <img
+        v-if="news.imageUrl && news.imageUrl.length > 0"
+        :src="news.imageUrl[0]"
+        alt="News Image"
+        class="w-full h-64 object-cover rounded-xl mb-2"
+        />
+      <div>
+        <span>{{ news.reporter }} • </span>
+        <span>{{ formatDate(news.reportDate) }}</span>
+      </div>
       <h2>{{ news.topic }}</h2>
-      <span>{{ news.shortDetail }}}</span>
+      <span>{{ news.shortDetail }}</span>
     </div>
-  </RouterLink>
-  </div>
+  <!-- </RouterLink> -->
 </template>
--- moo_kra_ta_news/src/types.ts
+++ moo_kra_ta_news/src/types.ts
@@ -5,7 +5,7 @@ export interface News{
     fullDetail: string
     reporter: string
     reportDate: string
-    imageUrl: []
+    imageUrl: string[] | undefined
     comments: []
 }
 
