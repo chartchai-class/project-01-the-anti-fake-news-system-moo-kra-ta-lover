57c817cc15271e112481342e67fd33cffbf16419
Chamor Lahuna <Chamor_l@cmu.ac.th>
2025-10-25 01:14:37 +0700

    refactor(news): create environment and link frontend with backend

--- moo_kra_ta_news/src/services/NewsService.ts
+++ moo_kra_ta_news/src/services/NewsService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://68a31457c5a31eb7bb1edde1.mockapi.io',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
