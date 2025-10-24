7945d5f0f97af87b5ca87fbd076f5e2bcd75cb4c
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2025-10-24 18:03:32 +0700

    feat(RegisterPage): added registerpage Html

--- moo_kra_ta_news/src/views/auth/RegisterView.vue
+++ moo_kra_ta_news/src/views/auth/RegisterView.vue
@@ -1,5 +1,77 @@
 <script setup lang="ts">
+import { CircleUserRound } from 'lucide-vue-next';
 </script>
 <template>
-  <H1>Register</H1>
+  <div class="mx-auto max-w-2xl">
+    <form>
+        <div>
+          <h2 class="text-base/7 font-semibold text-gray-900">Profile</h2>
+          <p class="mt-2 text-sm/6 text-gray-600">This information will be displayed publicly so be careful what you
+            share.</p>
+        </div>
+      <div class="space-y-8">
+        <div class="mt-8 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div class="sm:col-span-3">
+            <label for="first-name" class="block text-sm/6 font-medium text-gray-900">First name</label>
+            <div class="mt-2">
+              <input type="text" name="first-name" id="first-name" autocomplete="given-name"
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            </div>
+          </div>
+
+          <div class="sm:col-span-3">
+            <label for="last-name" class="block text-sm/6 font-medium text-gray-900">Last name</label>
+            <div class="mt-2">
+              <input type="text" name="last-name" id="last-name" autocomplete="family-name"
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            </div>
+          </div>
+
+        </div>
+
+        <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div class="sm:col-span-4">
+            <label for="username" class="block text-sm/6 font-medium text-gray-900">Email</label>
+            <div class="mt-2">
+              <div
+                class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                <div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6">Example@gmail.com</div>
+                <input type="text" name="email" id="email"
+                  class="block min-w-0 grow bg-white py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" />
+              </div>
+            </div>
+          </div>
+
+          <div class="sm:col-span-4">
+            <label for="username" class="block text-sm/6 font-medium text-gray-900">Password</label>
+            <div class="mt-2">
+              <div
+                class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                <input type="text" name="username" id="username"
+                  class="block min-w-0 grow bg-white py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                  placeholder="Password" />
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-span-full">
+            <label for="photo" class="block text-sm/6 font-medium text-gray-900">Photo</label>
+            <div class="mt-2 flex items-center gap-x-3">
+              <CircleUserRound class="size-12 text-gray-300" aria-hidden="true" />
+              <button type="button"
+                class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Change</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
+        <button type="submit"
+          class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+      </div>
+    </form>
+  </div>
 </template>
