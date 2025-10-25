8f7b8b3fe23a130aa7061850074a3bf73877cea6
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2025-10-25 13:34:50 +0700

    refactor(navbar): change to display signin when there is user and signout when there is no user

--- moo_kra_ta_news/src/components/NavBar.vue
+++ moo_kra_ta_news/src/components/NavBar.vue
@@ -26,23 +26,40 @@ const authStore = useAuthStore()
         </div>
       </div>
 
-      <div class="flex items-center gap-3">
+
+      <!-- Right-side buttons: Sign in / Sign out -->
+      <div class="flex items-center" v-if="!authStore.currentUserFirstName">
+        <!-- Sign in and Sign up button shown when not logged in -->
+        <div class="flex items-center gap-3">
+          <div
+            class="rounded-full px-4 py-2 text-sm font-medium bg-white border border-gray-300 text-gray-800 hover:bg-gray-100 shadow">
+            <router-link to="/logIn">
+              <button>
+                Sign in
+              </button>
+            </router-link>
+          </div>
+
+          <div
+            class="rounded-full px-4 py-2 text-sm font-medium bg-[#DD0B12] border border-gray-300 text-white hover:bg-[#af0000] shadow">
+            <router-link to="/register">
+              <button>
+                Sign Up
+              </button>
+            </router-link>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="flex items-center gap-3" v-if="authStore.currentUserFirstName">
         <RouterLink to="/">
           <button
-            class="flex items-center gap-2 bg-red-600 hover:bg-red-700 text-white font-medium px-4 py-2 rounded-xl transition"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-plus-lg"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"
-              />
+            class="flex items-center gap-2 bg-red-600 hover:bg-red-700 text-white font-medium px-4 py-2 rounded-xl transition">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg"
+              viewBox="0 0 16 16">
+              <path fill-rule="evenodd"
+                d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
             </svg>
             Submit News
           </button>
@@ -50,24 +67,13 @@ const authStore = useAuthStore()
 
         <RouterLink to="/">
           <button
-            class="flex items-center gap-2 border border-gray-300 text-gray-700 font-medium px-4 py-2 rounded-xl hover:bg-gray-100 transition"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-box-arrow-right"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
-              />
-              <path
-                fill-rule="evenodd"
-                d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
-              />
+            class="flex items-center gap-2 border border-gray-300 text-gray-700 font-medium px-4 py-2 rounded-xl hover:bg-gray-100 transition">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+              class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd"
+                d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
+              <path fill-rule="evenodd"
+                d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
             </svg>
             Sign Out
           </button>
