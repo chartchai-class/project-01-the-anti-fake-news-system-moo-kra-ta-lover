067f209b6ab1197b98be8101bf36a5b7f2d06d28
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2025-10-25 16:59:50 +0700

    feat(registerview): added logic to registerview page

--- moo_kra_ta_news/src/views/auth/RegisterView.vue
+++ moo_kra_ta_news/src/views/auth/RegisterView.vue
@@ -1,29 +1,72 @@
 <script setup lang="ts">
+import { useAuthStore } from '@/stores/auth';
 import { CircleUserRound } from 'lucide-vue-next';
+import { useField, useForm } from 'vee-validate';
+import { useRouter } from 'vue-router';
+import * as yup from 'yup';
+import InputText from '../../components/InputText.vue';
+
+
+const router = useRouter()
+
+const authStore = useAuthStore()
+
+const validationSchema = yup.object({
+  email: yup.string().required('The email is required'),
+  password: yup.string().required('The password is required')
+})
+const { errors, handleSubmit } = useForm({
+  validationSchema,
+  initialValues: {
+    email: '',
+    firstName: '',
+    lastName: '',
+    password: '',
+    image: ''
+  }
+})
+
+const { value: email } = useField<string>('email')
+const { value: firstName } = useField<string>('firstName')
+const { value: lastName } = useField<string>('lastName')
+const { value: image } = useField<string>('image')
+const { value: password } = useField<string>('password')
+
+const onSubmit = handleSubmit((values) => {
+  authStore.register(values.email, values.firstName, values.lastName,values.password,values.image)
+  .then(() => {
+    console.log('Register successful')
+    router.push({ name: 'login' })   
+  })
+  .catch((error) => {
+    setTimeout(() => {
+      messageStore.resetMessage()
+    }, 3000)
+    console.log('Register failed', error) 
+  });
+})
 </script>
 <template>
   <div class="mx-auto max-w-2xl">
-    <form>
-        <div>
-          <h2 class="text-base/7 font-semibold text-gray-900">Profile</h2>
-          <p class="mt-2 text-sm/6 text-gray-600">This information will be displayed publicly so be careful what you
-            share.</p>
-        </div>
+    <form @submit.prevent='onSubmit'>
+      <div>
+        <h2 class="text-base/7 font-semibold text-gray-900">Profile</h2>
+        <p class="mt-2 text-sm/6 text-gray-600">This information will be displayed publicly so be careful what you
+          share.</p>
+      </div>
       <div class="space-y-8">
         <div class="mt-8 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-3">
             <label for="first-name" class="block text-sm/6 font-medium text-gray-900">First name</label>
             <div class="mt-2">
-              <input type="text" name="first-name" id="first-name" autocomplete="given-name"
-                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              <InputText type="text" v-model="firstName" placeholder="First Name" :error="errors['firstname']" />
             </div>
           </div>
 
           <div class="sm:col-span-3">
             <label for="last-name" class="block text-sm/6 font-medium text-gray-900">Last name</label>
             <div class="mt-2">
-              <input type="text" name="last-name" id="last-name" autocomplete="family-name"
-                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              <InputText type="text" v-model="lastName" placeholder="last Name" :error="errors['lastname']" />
             </div>
           </div>
 
@@ -33,24 +76,14 @@ import { CircleUserRound } from 'lucide-vue-next';
           <div class="sm:col-span-4">
             <label for="username" class="block text-sm/6 font-medium text-gray-900">Email</label>
             <div class="mt-2">
-              <div
-                class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                <div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6">Example@gmail.com</div>
-                <input type="text" name="email" id="email"
-                  class="block min-w-0 grow bg-white py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" />
-              </div>
+              <InputText type="email" v-model="email" placeholder="Example.email.com" :error="errors['email']" />
             </div>
           </div>
 
           <div class="sm:col-span-4">
             <label for="username" class="block text-sm/6 font-medium text-gray-900">Password</label>
             <div class="mt-2">
-              <div
-                class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                <input type="text" name="username" id="username"
-                  class="block min-w-0 grow bg-white py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
-                  placeholder="Password" />
-              </div>
+              <InputText type="password" v-model="password" placeholder="Password" :error="errors['password']" />
             </div>
           </div>
 
