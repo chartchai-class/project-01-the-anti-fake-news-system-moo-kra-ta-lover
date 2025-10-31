2f9040e81095d40fa2458dfd2a270527fa9cd187
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-10-31 15:18:04 +0700

    style(homepage): update search input style

--- moo_kra_ta_news/src/components/home-page/SearchInput.vue
+++ moo_kra_ta_news/src/components/home-page/SearchInput.vue
@@ -9,7 +9,7 @@ function performSearch() {
 
 <template>
   <div class="w-full flex gap-2 items-center justify-center">
-    <input class="w-1/2 border border-gray-300 rounded-full p-2 bg-neutral-100 px-4" v-model="searchTerm" placeholder="Search any news that suit you" />
+    <input class="w-full border border-gray-300 rounded-full p-2 bg-neutral-100 px-4" v-model="searchTerm" placeholder="Search any news that suit you" />
     <button class="rounded-full px-4 py-2 text-sm font-medium bg-red-600 border border-gray-300 text-white hover:bg-red-700 shadow" @click="performSearch">Search</button>
   </div>
 </template>
